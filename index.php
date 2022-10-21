d861cfccf133a9109c39b30d32913c99954671b3
keepe <mavrin79@mail.ru>
2022-10-22 00:29:16 +0400

    First commit

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>Шредирование и полное уничтожение документов</title>
 </head>
@@ -16,7 +16,7 @@
     <section>
         <nav class="navbar navbar-expand-lg navbar-light bg_transparent pt-3 pb-3">
             <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#"><img src="/img/logo.png" alt="logo" width="80"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -37,21 +37,76 @@
     </section>
     <section class="bg_head">
         <div class="container">
-            <div class="row">
-
+            <div class="row align-items-center">
                 <div class="col-md-6">
                     <h1>Шредирование и полное уничтожение документов</h1>
                     <span class="sub-text mt-5">Наши специалисты занимаются созданием электронного архива для
                         предприятий любой величины.</span>
+                    <a href="" class="btn btn-primary btn-lg br-50 pl-5 pr-5 shadow">Подробнее</a>
                 </div>
                 <div class="col-md-6">
-                    <img src="/img/shredder.png" alt="шреддер" class="img-fluid">
+                    <img src="/img/bum.png" alt="шреддер" class="img-fluid">
                 </div>
 
             </div>
         </div>
     </section>
 </div>
+<section class="mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Уничтожение документов</h2>
+                <img src="/img/un.jpg" alt="Уничтожение документов" class="img-fluid mt-5">
+                <p>В любой организации, рано или поздно скапливается такое количество документов, что их становится
+                    просто негде хранить. Встает вопрос, что с ними делать? Можно конечно за баснословное оборудовать
+                    или снимать помещение для хранения, но лучше всего прибегнуть к уничтожению документов.</p>
+
+                <h3>Не стоит боятся</h3>
+                <p>Ничего страшного не случится, если уничтожать документы по закону. Каждый документ, будь то счет или
+                    приказ имеет свой срок хранения, согласно "Перечню типовых документов". И тут радостная новость,
+                    большинство документов имеет срок хранения не больше 5 лет.</p>
+                <h3>Как происходит процесс</h3>
+                <p>Сразу хочется спросить, как это сделать правильно. В архиве просматриваются все папки, далее к
+                    уничтожению отбираются документы срок которых истек. Составляется акт, утверждается руководителем.
+                    Последний этап документы вывозятся и уничтожаются.</p>
+                <h3>Обращайтесь к профессионалам</h3>
+                <p>Мы оказываем услуги по уничтожению документов с истекшими сроками. Благодаря огромному опыту и знанию
+                    архивного дела мы позволяем нашим заказчикам сэкономить средства на содержание архива и избежать
+                    лишних штрафов.</p>
+                <h3>Что включает в себя эта услуга</h3>
+                <ul>
+                    <li>Отбор документов к уничтожению;</li>
+                    <li>Оформление акта;</li>
+                    <li>Утилизация документов.</li>
+                </ul>
+                <h3>Скорость выполнения заказа</h3>
+                <p>Уничтожение документов проводиться опытными архивистами мы приступаем к работе на следующий день
+                    после заключения договора. Процесс утилизации 1 тонны бумаг занимает 10 минут.</p>
+
+                <h3>Итоги своевременного уничтожения</h3>
+                <p>Сокращается физический объем документов, что позволяет высвободить площади. Звоните нам по телефону:
+                    8 (495) 227-04-94 или пишите на info@archiv-s.ru мы поможем уничтожить документы правильно.</p>
+                <form class="form" id="recl2903" method="POST">
+                    <span class="title">Оставить заявку на консультацию</span>
+                    <div class="flex">
+                        <div class="form-group">
+                            <input type="text" placeholder="Ваше имя" name="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input placeholder="Номер телефона/email" name="contact" class="form-control">
+                        </div>
+                        <input type="button" class="purple-btn" onclick="call('recl2903')" value="Отправить">
+                        <input type="hidden" name="recaptcha_response" id="recaptchaResponse2903"
+                               value="03AIIukzjrgyG8aoQwMpVRVGD3ICYsSELMgIRjMzG4paxzrLzWYJMcG6SaDVNG9m92J7jyszEWz0UrLTQQuAc5oST8VymzZx9Q3VNbcysMmb8wqeLJ1u39j_pAP3c3PFYp4byj_RxTALqghgSo1eBe4dga7VR1mKwxgAufViz9v3VkLDlozGd8cPCCQ8TGEHCoKTjdGlidSo-todY6FqFa61RthZnBDqLxbLTnOj_ToecgNRCm9d5TPjezS7oFZN1kwDCl4UjRbOrIo6T8RZa25fiQ2JkoGWlHcI-DIm3x04dHgsMHwQQcHjn-oIZPi1ylAt9B_Dc8gPzeLdt0FapfjUHH_WxNmt0w6L5VTmnhcS-p6GFeK_qKe9bMgmvVZ1x7AW7i9eUql0amdSi4ECccaQyNxx79mAC_QUIkChhSFhRGIst-Fl5ZicY1j33pvSR9uDsI4VhNMm122XW2iRbkU3FGVwx1Ux5pQ1bzb8KDvZUKTm_qbjJGHb0FqDTKCWuQFNlwqJhmDKU4">
+                        <input type="hidden" name="wintype" value="aftertext">
+                    </div>
+                    <div id="recl2903res"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
